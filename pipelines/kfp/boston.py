b5258c635bba444313057eb94506a101122a1dcb
Student User <student@workstation.lab.example.com>
2025-02-20 11:02:18 -0500

    dd

--- pipelines/kfp/boston.py
+++ pipelines/kfp/boston.py
@@ -1,40 +1,89 @@
 from kfp import dsl, compiler
-from kfp.dsl import Input, Output, Dataset
+from kfp.dsl import Input, Output, Dataset, Artifact, Model
+
+DSI = 'quay.io/modh/cuda-notebooks:cuda-jupyter-tensorflow-ubi9-python-3.11-20250213-b23e7ed'
 
 @dsl.component(
-        base_image="image-registry.openshift-image-registry.svc:5000/redhat-ods-applications/tensorflow"
+        base_image=DSI
         )
 def gather_data(data: Output[Dataset]):
     import pandas as pd
-    ds = pd.read_csv('boston_housing.csv')
-    ds.to_csv(data.path)
+    import os
+    import io
+    import boto3
+    import numpy as np
+
+    s3 = boto3.client(
+        "s3",
+        'us-east-1',
+        aws_access_key_id='minio',
+        aws_secret_access_key='minio123',
+        endpoint_url='https://minio-api-minio.apps.ocp4.example.com'
+    )
+
+    objects = s3.list_objects_v2(Bucket='bostonhousing')
+
+# output the name of each object within the bucket
+    for obj in objects["Contents"]:
+        print(obj["Key"])
+        s3.download_file('bostonhousing', obj["Key"], obj["Key"], )
+       
+        ds = pd.read_csv('boston_housing.csv')
+        ds.to_csv(data.path)
 
 @dsl.component(
-        base_image="image-registry.openshift-image-registry.svc:5000/redhat-ods-applications/tensorflow"
+        base_image=DSI
         )
 def clean_data(data_in: Input[Dataset], data_out: Output[Dataset]):
     import pandas as pd
-    ds = pd.read_csv(data_in.path)
-    ds.to_csv(data_out.path)
+    df = pd.read_csv(data_in.path)
+    nhdi = df[(df['PRICE'] > 45) | (df['RM'] < 4)].index
+    df.drop(nhdi)
+    df.to_csv(data_out.path)
 
 @dsl.component(
-        base_image="image-registry.openshift-image-registry.svc:5000/redhat-ods-applications/tensorflow"
+        base_image=DSI
         )
-def train(data: Input[Dataset]):
+def train(data: Input[Dataset], model_out: Output[Model]):
     import pandas as pd
-    ds = pd.read_csv(data.path)
+    import tensorflow as tf
+    import numpy as np
+
+    df = pd.read_csv(data.path)
+    x_train,y_train = df.RM.values[:-100], df.PRICE.values[:-100]
+    x_test,y_test = df.RM.values[-100:], df.PRICE.values[-100:]
+    model = tf.keras.Sequential()
+    model.add(tf.keras.layers.Input(shape=(1,)))
+    model.add(tf.keras.layers.Normalization())
+    model.add(tf.keras.layers.Dense(1))
+    model.compile(optimizer='rmsprop',loss=tf.keras.losses.MeanSquaredError(),metrics=[tf.keras.metrics.RootMeanSquaredError()])
+    history = model.fit(x_train,y_train,validation_split=0.1,epochs=300,verbose=2)
+    
+    model_out.uri = model_out.uri + '.keras'
+    model.save(model_out.path)
 
 @dsl.component(
-        base_image="image-registry.openshift-image-registry.svc:5000/redhat-ods-applications/tensorflow"
+        base_image=DSI
         )
-def infer():
-    print ('Infering')
+def infer(model_in: Input[Model]):
+    import numpy as np
+    import tensorflow as tf
+
+    model = tf.keras.models.load_model(model_in.path)
+
+    x = tf.linspace(4,12,10)
+    y = model.predict(x)
+
+    print('-------------------- Predictions ------------------------')
+    print(y)
+
 
 @dsl.pipeline(name='boston')
 def boston_pipeline():
-    gather_data(data)
-    clean_data(data, clean_data)
-    train(data)
+   task1 = gather_data()
+   task2 = clean_data(data_in = task1.outputs['data'])
+   task3 = train(data = task2.outputs['data_out'])
+   infer(model_in = task3.outputs['model_out'])
 
 
 
